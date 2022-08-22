3ff2d3e5ea75fedd47019da1d162ef7202c994bd
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-08-22 20:23:59 -0300

    Calculadora soma
    Implementando calculadora de soma

--- javascript/aula1-JS/js/meuPrimeiroJs.js
+++ javascript/aula1-JS/js/meuPrimeiroJs.js
@@ -1,1 +1,19 @@
-alert('Aulão do Cleyton!');
+// alert('Aulão do Cleyton!');
+// console.log('Testando console');
+// prompt('Digite seu nome:');
+
+// var não guarda escopo (pode redeclarar a mesma variável), let tem estrutura de escopo, const não pretendemos alterar.
+// var a = 1;
+// var b = 1;
+// var a = 5;
+// console.log(a + b);
+
+//Calculadora 1 Soma
+{
+    const mensagemBonita = 'Sucesso, você somou 2 números!';
+    let recebePrimeiroNumero = prompt('Digite o primeiro número');
+    let recebeSegundoNumero = prompt('Digite o segundo número');
+
+    console.log(mensagemBonita);
+    console.log(recebePrimeiroNumero + recebeSegundoNumero);
+}
