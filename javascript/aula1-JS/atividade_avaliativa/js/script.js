f136620bb66a0ad3e92c67ff826992dd08d27cf7
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-08-23 11:06:12 -0300

    Atividade avaliativa aula01 javascript
    Atividade de soma e receber valor com o prompt

---
+++ javascript/aula1-JS/atividade_avaliativa/js/script.js
@@ -0,0 +1,7 @@
+let primeiroNumero = 10;
+let segundoNumero = 5;
+let resultadoSoma = primeiroNumero + segundoNumero;
+console.log('O resultado da soma é ' + resultadoSoma);
+
+let valorUsuario = prompt('Digite um valor');
+console.log(valorUsuario);
