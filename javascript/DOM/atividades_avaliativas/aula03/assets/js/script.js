c738b7cbb0e8c095651319158aa1532c47cf6db6
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-23 10:05:17 -0300

    refatorando exercicio

--- javascript/DOM/atividades_avaliativas/aula03/assets/js/script.js
+++ javascript/DOM/atividades_avaliativas/aula03/assets/js/script.js
@@ -8,23 +8,42 @@ let feedbackLastName = document.getElementById('feedback-lastName');
 let feedbackCpf = document.getElementById('feedback-cpf');
 
 submitButton.onclick = () => {
-    if (inputName.value === '') {
+    if (validateName()) {
+        if (validateLastName()) {
+            if (validateCpf()) {
+                setTimeout(resetFields, 2000);
+            }
+        }
+    }
+};
+
+function validateName() {
+    let name = inputName.value;
+    if (name === '') {
         feedbackName.style.display = 'block';
         inputName.focus();
         return;
     } else {
         feedbackName.style.display = 'none';
     }
+    return true;
+}
 
-    if (inputLastName.value === '') {
+function validateLastName() {
+    let lastName = inputLastName.value;
+    if (lastName === '') {
         feedbackLastName.style.display = 'block';
         inputLastName.focus();
         return;
     } else {
         feedbackLastName.style.display = 'none';
     }
+    return true;
+}
 
-    if (inputCpf.value === '') {
+function validateCpf() {
+    let cpf = inputCpf.value;
+    if (cpf === '') {
         feedbackCpf.style.display = 'block';
         feedbackCpf.textContent = 'Campo obrigatório!';
         inputCpf.focus();
@@ -33,25 +52,25 @@ submitButton.onclick = () => {
         feedbackCpf.style.display = 'none';
     }
 
-    if (!isNaN(inputCpf.value) && String(inputCpf.value).length === 11) {
+    if (!isNaN(cpf) && String(cpf).length === 11) {
         feedbackCpf.style.display = 'block';
         feedbackCpf.style.color = 'green';
         feedbackCpf.textContent = 'Sucesso!';
         alert('Dados enviados com sucesso!');
-        setTimeout(resetFields, 2000);
     } else {
         feedbackCpf.style.display = 'block';
         feedbackCpf.textContent = 'Cpf inválido!';
         inputCpf.focus();
         return;
     }
-};
+    return true;
+}
 
 function resetFields() {
     feedbackLastName.style.display = 'none';
     feedbackCpf.style.display = 'none';
     feedbackName.style.display = 'none';
-    feedbackCpf.style.color = 'red';
+    feedbackCpf.style.color = '#ca3131';
     inputName.value = '';
     inputLastName.value = '';
     inputCpf.value = '';
