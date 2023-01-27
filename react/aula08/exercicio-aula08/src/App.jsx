37fdb6fcd0516fef50beacf89e8aa1413a4e0b04
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-01-27 11:46:12 -0300

    Atividade Avaliativa aula 08 - React Router

---
+++ react/aula08/exercicio-aula08/src/App.jsx
@@ -0,0 +1,13 @@
+import Breadcrumb from './components/Breadcrumb/Breadcrumb';
+import { Router } from './router';
+
+function App() {
+    return (
+        <div>
+            <Breadcrumb />
+            <Router />
+        </div>
+    );
+}
+
+export default App;
